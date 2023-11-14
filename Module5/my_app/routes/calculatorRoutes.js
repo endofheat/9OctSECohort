5ade88bb7171549fef8275e49edc315ff329795e
endofheat <endofheat97@gmail.com>
2023-11-14 20:42:26 +1300

    update

--- Module5/my_app/routes/calculatorRoutes.js
+++ Module5/my_app/routes/calculatorRoutes.js
@@ -3,10 +3,40 @@ const router = express.Router();
 
 // new route for adding two numbers
 router.get('/add', (req, res) => {
-    let num1;
-    console.log(req.query);
-    res.send(req.query);
+    let firstNum = parseInt(req.query.num1);
+    let secondNum = parseInt(req.query.num2);
+    let addResult = firstNum + secondNum;
+    console.log(addResult);
+    res.status(200);
+    res.json({result: addResult});
 
+});
+
+router.get('/subtract', (req, res) => {
+    let firstNum = parseInt(req.query.num1);
+    let secondNum = parseInt(req.query.num2);
+    let addResult = firstNum - secondNum;
+    console.log(addResult);
+    res.status(200);
+    res.json({result: addResult});
+});
+
+router.get('/divide', (req, res) => {
+    let firstNum = parseInt(req.query.num1);
+    let secondNum = parseInt(req.query.num2);
+    let addResult = firstNum / secondNum;
+    console.log(addResult);
+    res.status(200);
+    res.json({result: addResult});
+});
+
+router.get('/multiply', (req, res) => {
+    let firstNum = parseInt(req.query.num1);
+    let secondNum = parseInt(req.query.num2);
+    let addResult = firstNum * secondNum;
+    console.log(addResult);
+    res.status(200);
+    res.json({result: addResult});
 });
 
 module.exports = router;
