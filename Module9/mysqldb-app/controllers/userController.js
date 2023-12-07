445b0562e4ce28117114fa03e338a802070f0a2c
endofheat <endofheat97@gmail.com>
2023-12-08 11:54:19 +1300

    fixed some typo

--- Module9/mysqldb-app/controllers/userController.js
+++ Module9/mysqldb-app/controllers/userController.js
@@ -1,5 +1,6 @@
 "use strict";
 const Models = require("../models");
+
 const getUsers = (res) => {
   Models.User.findAll({})
     .then(function (data) {
@@ -10,40 +11,41 @@ const getUsers = (res) => {
       res.send({result:500, error: err.message});
     });
 };
-const createUsers = (data, res) => {
+const createUser = (data, res) => {
   Models.User.create(data)
     .then(function (data) {
       res.send({ result: 200, data: data });
     })
     .catch((err) => {
-      throw err;
+      console.log(err);
+      res.send({result:500, error: err.message});
     });
 };
-const updateUsers = (data, res) => {
+const updateUser = (data, res) => {
     Models.User.update(req.body, {where: {id:req.params.id}})
       .then(function (data) {
         res.send({ result: 200, data: data });
       })
       .catch((err) => {
         console.log(err);
-      res.send({result:500, error: err.message});
+        res.send({result:500, error: err.message});
       });
   };
 
-  const deleteUsers = (data, res) => {
+  const deleteUser = (data, res) => {
     Models.User.destroy({where: {id:req.params.id}})
       .then(function (data) {
         res.send({ result: 200, data: data });
       })
       .catch((err) => {
         console.log(err);
-      res.send({result:500, error: err.message});
+        res.send({result:500, error: err.message});
       });
   };
 
 module.exports = {
   getUsers,
-  createUsers,
-  updateUsers,
-  deleteUsers,
+  createUser,
+  updateUser,
+  deleteUser,
 };
